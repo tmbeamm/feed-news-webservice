75b12f4f5c386b3e61ba312b50c5b90dab1c417a
6ulico <sontaya.foo@gmail.com>
2019-01-06 16:20:53 +0700

    modify FeedNewsWebserviceApplication

--- src/main/java/com/example/api/FeedNewsWebserviceApplication.java
+++ src/main/java/com/example/api/FeedNewsWebserviceApplication.java
@@ -1,36 +1,14 @@
 package com.example.api;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.entity.FeedTopic;
-import com.example.repository.FeedTopicRepository;
-import com.example.services.FeedTopicService;
-
 @SpringBootApplication
-public class FeedNewsWebserviceApplication  implements CommandLineRunner{
+public class FeedNewsWebserviceApplication{
 	
-	@Autowired
-	FeedTopicService feedTopicService;
 	public static void main(String[] args) {
 		SpringApplication.run(FeedNewsWebserviceApplication.class, args);
 	}
-    @Override
-
-    public void run(String... arg0) throws Exception {
-    	try {
-    		List<FeedTopic> list = feedTopicService.listData();
-			System.out.println("Pass");
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println("Fail");
-			e.printStackTrace();
-		}
-    	
-    }
+   
 }
 
